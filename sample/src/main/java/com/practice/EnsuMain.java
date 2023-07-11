58fb7c7bce09238bf11c12e12ffa5c2f6a9645fb
LAPTOP-414JFBU8\81903 <takahiro.nishimura@mt-square.co.jp>
2023-07-11 12:41:14 +0900

    演習問題1作成

--- sample/src/main/java/com/practice/EnsuMain.java
+++ sample/src/main/java/com/practice/EnsuMain.java
@@ -1,33 +1,128 @@
 package com.practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EnsuMain {
 
 	public static void main(String[] args) {
 		EnsuService ensuService = new EnsuService();
-		
-		//演習問題1
-		EnsuService.printHello();
-		//演習問題2
-		EnsuService.printPI();
-		//演習問題3
-		EnsuService.printRandomMessage();
-		//演習問題4
-		EnsuService.printMessage("Hello");
-		//演習問題5
-		EnsuService.printCircleArea(2.0);
-		//演習問題6
-		EnsuService.printRandomMessage("社長");
-		//演習問題7
-		EnsuService.printMessage("Hello",5);
-		//演習問題8
-		EnsuService.printRectangleArea(2.0,3.0);
-		//演習問題9
-		EnsuService.printMaxValue(2.0,3.0,4.0);
-		EnsuService.printMaxValue(4.0,3.0,5.0);
-		EnsuService.printMaxValue(3.0,6.0,5.0);
-		EnsuService.printMaxValue(5.0,6.0,4.0);
-		EnsuService.printMaxValue(7.0,3.0,5.0);
-		EnsuService.printMaxValue(7.0,6.0,5.0);
-	}
 
+		// 演習問題1
+		EnsuService.printHello();
+		// 演習問題2
+		EnsuService.printPI();
+		// 演習問題3
+		EnsuService.printRandomMessage();
+		// 演習問題4
+		EnsuService.printMessage("Hello");
+		// 演習問題5
+		EnsuService.printCircleArea(2.0);
+		// 演習問題6
+		EnsuService.printRandomMessage("社長");
+		// 演習問題7
+		EnsuService.printMessage("Hello", 5);
+		// 演習問題8
+		EnsuService.printRectangleArea(2.0, 3.0);
+		// 演習問題9
+		EnsuService.printMaxValue(2.0, 3.0, 4.0);
+		EnsuService.printMaxValue(4.0, 3.0, 5.0);
+		EnsuService.printMaxValue(3.0, 6.0, 5.0);
+		EnsuService.printMaxValue(5.0, 6.0, 4.0);
+		EnsuService.printMaxValue(7.0, 3.0, 5.0);
+		EnsuService.printMaxValue(7.0, 6.0, 5.0);
+		// 演習問題10
+		System.out.println(EnsuService.getMessage());
+		// 演習問題11
+		System.out.println(EnsuService.getWeatherForecast());
+		// 演習問題12
+		System.out.println("2の平方根は" + EnsuService.getSquareRootOf2());
+		// 演習問題13
+		System.out.println(EnsuService.getRandomMessage("社長"));
+		// 演習問題14
+		System.out.println(EnsuService.getAbsoluteValue(5.2));
+		System.out.println(EnsuService.getAbsoluteValue(-3.6));
+		// 演習問題15
+		System.out.println(EnsuService.isEvenNumber(5));
+		System.out.println(EnsuService.isEvenNumber(10));
+		// 演習問題16
+		System.out.println("最小値は" + EnsuService.getMinValue(7.7, 11.1));
+		System.out.println("最小値は" + EnsuService.getMinValue(7.7, -11.1));
+		// 演習問題17
+		System.out.println(EnsuService.isSameAbsoluteValue(1, 2));
+		System.out.println(EnsuService.isSameAbsoluteValue(2, 1));
+		System.out.println(EnsuService.isSameAbsoluteValue(100, 100));
+		// 演習問題18
+		System.out.println(EnsuService.getMessage("社長", true));
+		System.out.println(EnsuService.getMessage("社長", false));
+		// 演習問題19
+		int[] numberList = { 10, 50, 1, 20, 15, 5 };
+		System.out.println("←の数値の中で最小値は" + EnsuService.getMinValue(numberList));
+		// 演習問題20
+		double[] doubleList = { 10.0, 50.0, 20.0 };
+		System.out.println("←の平均値は" + EnsuService.getAverage(doubleList));
+
+		// 演習問題21
+		String[] messageList = { "Hello", "hoge", "おはようございます。", "こんにちは" };
+		System.out.println("←最大文字列は" + EnsuService.getLongestString(messageList));
+		// 演習問題22
+		System.out.println("距離は" + EnsuService.getDistanceFromOrigin(new Point(3.0, 2.0)));
+		// 演習問題23
+		System.out.println("距離は" + EnsuService.getDistanceBetweenTwoPoints(new Point(0.0, 0.0), new Point(3.0, 2.0)));
+		// 演習問題24
+		List<Point> pointList = new ArrayList<Point>();
+		Point point = new Point(0.0, 0.0);
+		Point point2 = new Point(4.0, 5.0);
+		Point point3 = new Point(2.0, 1.0);
+		Point point4 = new Point(10.0, 10.0);
+
+		pointList.add(point);
+		pointList.add(point2);
+		pointList.add(point3);
+		pointList.add(point4);
+
+		Point pointAverage = EnsuService.getBarycenter(pointList);
+		System.out.println("平均座標はx=" + pointAverage.x + "y=" + pointAverage.y);
+
+		// 演習問題25
+		EnsuService.printMessage(new Person("社長",36));
+		// 演習問題26
+		System.out.println(EnsuService.isAdult(new Person("大人",20)));
+		System.out.println(EnsuService.isAdult(new Person("子供",19)));
+		
+		// 演習問題27
+		List<Person> personList = new ArrayList<Person>();
+		Person person = new Person("社長",36);
+		Person person1 = new Person("西村",35);
+		Person person2 = new Person("大橋",36);
+		Person person3 = new Person("富武",30);
+		Person person4 = new Person("堀江",30);
+		
+		personList.add(person);
+		personList.add(person1);
+		personList.add(person2);
+		personList.add(person3);
+		personList.add(person4);
+		
+		Person minPerson = EnsuService.getYoungestPerson(personList);
+		System.out.println("メンバーの最少年齢は" + minPerson.getName() + "さんで年齢は" + minPerson.getAge() + "です");
+		
+		// 演習問題28
+		System.out.println("<<<<<<<<<<<<<<演習問題28>>>>>>>>>>>>>>>>>>>");
+		Person person5 = new Person();
+		person5.setName("渡辺");
+		System.out.println(person5.getName());
+		
+		// 演習問題29
+		System.out.println("<<<<<<<<<<<<<<演習問題29>>>>>>>>>>>>>>>>>>>");
+		person5.setAge(35);
+
+		Person person6 = new Person();
+		person6.setName("hoge");
+		person6.setAge(-1);
+
+		System.out.println(person5.getName()+ "は" + person5.getAge()+ "歳です");
+		System.out.println(person6.getName()+ "は" + person6.getAge()+ "歳です");
+		
+	}
 }
--- sample/src/main/java/com/practice/EnsuService.java
+++ sample/src/main/java/com/practice/EnsuService.java
@@ -1,5 +1,9 @@
 package com.practice;
 
+import java.util.List;
+
+import org.hibernate.type.descriptor.jdbc.H2FormatJsonJdbcType;
+
 public class EnsuService {
 
 	public static void printHello() {
@@ -9,75 +13,233 @@ public class EnsuService {
 
 	public static void printPI() {
 		System.out.println("<<<<<<<<<<<<<<演習問題2>>>>>>>>>>>>>>>>>>>");
-		System.out.println(Math.PI);		
+		System.out.println(Math.PI);
 	}
 
 	public static void printRandomMessage() {
 		System.out.println("<<<<<<<<<<<<<<演習問題3>>>>>>>>>>>>>>>>>>>");
-		int n = (int)(3 * Math.random());
+		int n = (int) (3 * Math.random());
 		if (n == 0) {
 			System.out.println("こんばんは");
-		}else if (n == 1) {
+		} else if (n == 1) {
 			System.out.println("こんにちは");
-		}else if  (n == 2){
+		} else if (n == 2) {
 			System.out.println("おはよう");
 		}
-		
+
 	}
 
 	public static void printMessage(String message) {
 		System.out.println("<<<<<<<<<<<<<<演習問題4>>>>>>>>>>>>>>>>>>>");
-		System.out.println(message);		
+		System.out.println(message);
 	}
 
 	public static void printCircleArea(double radius) {
 		System.out.println("<<<<<<<<<<<<<<演習問題5>>>>>>>>>>>>>>>>>>>");
-		System.out.println("半径2の円の面積は" + radius * radius* Math.PI);
+		System.out.println("半径2の円の面積は" + radius * radius * Math.PI);
 	}
 
 	public static void printRandomMessage(String name) {
 		System.out.println("<<<<<<<<<<<<<<演習問題6>>>>>>>>>>>>>>>>>>>");
-		int n = (int)(3 * Math.random());
-		if (n == 0) {
-			System.out.println("こんばんは" + name + "さん" );
-		}else if (n == 1) {
-			System.out.println("こんにちは"+ name + "さん" );
-		}else if  (n == 2){
-			System.out.println("おはよう"+ name + "さん" );
-		}
-		
+		int n = (int) (3 * Math.random());
+		String[] message = { "こんばんは", "こんにちは", "おはよう" };
+		System.out.println(message[n] + name + "さん");
 	}
 
 	public static void printMessage(String message, int count) {
 		System.out.println("<<<<<<<<<<<<<<演習問題7>>>>>>>>>>>>>>>>>>>");
-		for (int i = 0  ; i <count ;i++) {
-			System.out.println(message);		
+		for (int i = 0; i < count; i++) {
+			System.out.println(message);
 		}
 	}
 
 	public static void printRectangleArea(double height, double width) {
 		System.out.println("<<<<<<<<<<<<<<演習問題8>>>>>>>>>>>>>>>>>>>");
-		System.out.println("半径2の円の面積は" + height * width * 1/2);
+		System.out.println("半径2の円の面積は" + height * width * 1 / 2);
 
 	}
 
 	public static void printMaxValue(double a, double b, double c) {
 		System.out.println("<<<<<<<<<<<<<<演習問題9>>>>>>>>>>>>>>>>>>>");
 		if (a > b) {
-			if(a > c) {
+			if (a > c) {
 				System.out.println(a);
-				return ;
+				return;
 			}
 			System.out.println(c);
-			return ;
-		} else if(b > c) {
+			return;
+		} else if (b > c) {
 			System.out.println(b);
-			return ;
+			return;
 		}
-		
+
 		System.out.println(c);
 	}
 
+	public static String getMessage() {
+		System.out.println("<<<<<<<<<<<<<<演習問題10>>>>>>>>>>>>>>>>>>>");
+		return "よろしくお願いいたします";
+	}
 
+	public static String getWeatherForecast() {
+		System.out.println("<<<<<<<<<<<<<<演習問題11>>>>>>>>>>>>>>>>>>>");
+		int n = (int) (3 * Math.random());
+		int m = (int) (4 * Math.random());
 
+		String[] message1 = { "今日", "明日", "明後日" };
+		String[] message2 = { "晴れ", "曇り", "雨", "雪" };
+
+		return message1[n] + "は" + message2[m];
+	}
+
+	public static double getSquareRootOf2() {
+		System.out.println("<<<<<<<<<<<<<<演習問題12>>>>>>>>>>>>>>>>>>>");
+		double d = Math.sqrt(2.0);
+		return d;
+	}
+
+	public static String getRandomMessage(String name) {
+		System.out.println("<<<<<<<<<<<<<<演習問題13>>>>>>>>>>>>>>>>>>>");
+		int n = (int) (3 * Math.random());
+		String[] message = { "こんばんは", "こんにちは", "おはよう" };
+		return message[n] + name + "さん";
+	}
+
+	public static double getAbsoluteValue(double value) {
+		System.out.println("<<<<<<<<<<<<<<演習問題14>>>>>>>>>>>>>>>>>>>");
+		return Math.abs(value);
+	}
+
+	public static boolean isEvenNumber(int value) {
+		System.out.println("<<<<<<<<<<<<<<演習問題15>>>>>>>>>>>>>>>>>>>");
+		// return value % 2 == 0; ←これだけでOK
+		if (value % 2 == 0) {
+			System.out.print(value + "は偶数");
+			return true;
+		}
+		System.out.print(value + "は奇数");
+		return false;
+	}
+
+	public static double getMinValue(double a, double b) {
+		System.out.println("<<<<<<<<<<<<<<演習問題16>>>>>>>>>>>>>>>>>>>");
+		return Math.min(a, b);
+	}
+
+	public static boolean isSameAbsoluteValue(int i, int j) {
+		System.out.println("<<<<<<<<<<<<<<演習問題17>>>>>>>>>>>>>>>>>>>");
+		// return i==j; ←これだけでOK
+		if (i == j) {
+			System.out.print(i + "と" + j + "は同じ値です");
+			return true;
+		}
+		System.out.print(i + "と" + j + "は違う値です");
+		return false;
+	}
+
+	public static String getMessage(String name, boolean isKid) {
+		System.out.println("<<<<<<<<<<<<<<演習問題18>>>>>>>>>>>>>>>>>>>");
+		if (isKid) {
+			return "こんにちは。" + name + "ちゃん";
+		}
+		return "こんにちは。" + name + "さん";
+	}
+
+	public static int getMinValue(int[] array) {
+		System.out.println("<<<<<<<<<<<<<<演習問題19>>>>>>>>>>>>>>>>>>>");
+		int min = array[0];
+		System.out.print(min + " ");
+		for (int i = 1; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+			if (min > array[i]) {
+				min = array[i];
+			}
+		}
+
+		return min;
+	}
+
+	public static double getAverage(double[] array) {
+		System.out.println("<<<<<<<<<<<<<<演習問題20>>>>>>>>>>>>>>>>>>>");
+		double sum = 0.0;
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+			sum += array[i];
+		}
+
+		return sum / array.length;
+	}
+
+	public static String getLongestString(String[] array) {
+		System.out.println("<<<<<<<<<<<<<<演習問題21>>>>>>>>>>>>>>>>>>>");
+		String message = array[0];
+		System.out.print(message + " ");
+		for (int i = 1; i < array.length; i++) {
+			System.out.print(array[i] + " ");
+			if (array[i].length() > message.length()) {
+				message = array[i];
+			}
+		}
+		return message;
+	}
+
+	public static double getDistanceFromOrigin(Point p) {
+		System.out.println("<<<<<<<<<<<<<<演習問題22>>>>>>>>>>>>>>>>>>>");
+		double x = p.x;
+		double y = p.y;
+		return Math.sqrt(x * x + y * y);
+	}
+
+	public static double getDistanceBetweenTwoPoints(Point p0, Point p1) {
+		System.out.println("<<<<<<<<<<<<<<演習問題23>>>>>>>>>>>>>>>>>>>");
+		double x0 = p0.x;
+		double x1 = p1.x;
+		double y0 = p0.y;
+		double y1 = p1.y;
+
+		return Math.sqrt((x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1));
+	}
+
+	public static Point getBarycenter(List<Point> points) {
+		System.out.println("<<<<<<<<<<<<<<演習問題24>>>>>>>>>>>>>>>>>>>");
+		double x = 0.00;
+		double y = 0.00;
+		for (Point point : points) {
+			x += point.x;
+			y += point.y;
+		}
+
+		return new Point(x / points.size(), y / points.size());
+	}
+
+	public static void printMessage(Person person) {
+		System.out.println("<<<<<<<<<<<<<<演習問題25>>>>>>>>>>>>>>>>>>>");
+		System.out.println("こんにちは" + person.getName() + "さん");
+	}
+
+	public static boolean isAdult(Person person) {
+		System.out.println("<<<<<<<<<<<<<<演習問題26>>>>>>>>>>>>>>>>>>>");
+		if (person.getAge() >= 20) {
+			System.out.print("大人");
+			return true;
+		}
+		System.out.print("子供");
+		return false;
+	}
+
+	public static Person getYoungestPerson(List<Person> personList) {
+		System.out.println("<<<<<<<<<<<<<<演習問題27>>>>>>>>>>>>>>>>>>>");
+		int minAge = personList.get(0).getAge();
+		String minName = personList.get(0).getName();
+
+		for(Person person: personList) {
+			int age =person.getAge();
+			if(minAge >= person.getAge()) {
+				minAge = age;
+				minName = person.getName();
+			}
+		}
+		
+		return new Person(minName,minAge);
+	}
 }
